e3453718b54eb715b8a564d525a5f9dfd7014ff2
xzh <1666384464@qq.com>
2026-09-18 01:42:22 +0800

    feat: add agent service goal DAG and board projection R7

--- agent_service/__init__.py
+++ agent_service/__init__.py
@@ -1,6 +1,17 @@
 """Ordivon Agent Service clean-room kernel."""
 
 from .evidence import AgentServiceR6, RuntimeArtifactReader, VerificationRecord
+from .goals import (
+    AgentServiceR7,
+    BoardAdapter,
+    BoardProjectionReceipt,
+    GoalGraphMutationGuard,
+    GoalStore,
+    GoalTaskLinkStore,
+    TaskDependencyStore,
+    TaskReadinessProjector,
+)
+from .host_board import HostBoardMcpAdapter
 from .runtime_mcp import RuntimeMcpAdapter, RuntimeMcpArtifactReader, RuntimeMcpHttpClient
 from .slice1 import AgentServiceSlice1, CarrierProviderAdapter, HostAdapter, ProviderObservation
 from .task_runtime import (
@@ -15,8 +26,15 @@ __all__ = [
     "AgentServiceSlice1",
     "AgentServiceR5",
     "AgentServiceR6",
+    "AgentServiceR7",
+    "BoardAdapter",
+    "BoardProjectionReceipt",
     "CarrierProviderAdapter",
+    "GoalGraphMutationGuard",
+    "GoalStore",
+    "GoalTaskLinkStore",
     "HostAdapter",
+    "HostBoardMcpAdapter",
     "ProviderObservation",
     "RuntimeAdapter",
     "RuntimeArtifactDescriptor",
@@ -26,5 +44,7 @@ __all__ = [
     "RuntimeMcpAdapter",
     "RuntimeMcpArtifactReader",
     "RuntimeMcpHttpClient",
+    "TaskDependencyStore",
+    "TaskReadinessProjector",
     "VerificationRecord",
 ]
